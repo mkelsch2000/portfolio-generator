88241c09112b311cf2b0fe3e58797a078a955feb
Matthew Kelsch <95149182+mkelsch2000@users.noreply.github.com>
2022-02-11 18:27:08 -0700

    added HTML generating functionality

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
+const fs = require("fs");
+
+const generatePage = require("./src/page-template.js");
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-// Notice the lack of parentheses around the "profileDataArr" parameter?
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
+const [name, github] = profileDataArgs;
 
-  console.log("================")
+fs.writeFile("index.html", generatePage(name, github), err => {
+  if (err) throw err;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
 
-printProfileData(profileDataArgs);
+// // Notice the lack of parentheses around the "profileDataArr" parameter?
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i++) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log("================")
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
