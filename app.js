8c0af11b978fe4ebac6dd561117da8763ae53981
Matthew Kelsch <95149182+mkelsch2000@users.noreply.github.com>
2022-02-12 14:04:59 -0700

    added answer validation

--- app.js
+++ app.js
@@ -15,12 +15,28 @@ const promptUser = () => {
     {
       type: "input",
       name: "name",
-      message: "what is your name?"
+      message: "what is your name? (Required)",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      }
     },
     {
       type: "input",
       name: "github",
-      message: "Enter your GitHub Username"
+      message: "Enter your GitHub Username. (Required)",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub Username!");
+          return false;
+        }
+      }
     },
     {
       type: "input",
@@ -51,7 +67,15 @@ const promptProject = portfolioData => {
     {
       type: "input",
       name: "description",
-      message: "Provide a description of the project (Required)"
+      message: "Provide a description of the project (Required)",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please give your project a description!");
+          return false;
+        }
+      }
     },
     {
       type: "checkbox",
@@ -62,7 +86,15 @@ const promptProject = portfolioData => {
     {
       type: "input",
       name: "link",
-      message: "Enter the GitHub link to your project. (Required)"
+      message: "Enter the GitHub link to your project. (Required)",
+      validate: nameInput => {
+        if (nameInput) {
+          return true; 
+        } else {
+          console.log("Please provide the link to your GitHub!");
+          return false;
+        }
+      }
     },
     {
       type: "confirm",
